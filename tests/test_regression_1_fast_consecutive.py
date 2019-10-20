d5527125b811860c5b93995015e0571536303770
matt <syan4@ualberta.ca>
2019-10-19 18:43:30 -0600

    BUILD

--- tests/test_regression_1_fast_consecutive.py
+++ tests/test_regression_1_fast_consecutive.py
@@ -26,5 +26,4 @@ def test_consecutive_analyses_fast(cree_analyzer):
     cree_analyzer.feed_in_bulk_fast(["acâ"])["acâ"]
     cree_analyzer.feed_in_bulk_fast(["acâh"])["acâh"]
     cree_analyzer.feed_in_bulk_fast(["acâhk"])["acâhk"]
-    cree_analyzer.feed_in_bulk_fast(["acâhko"])["acâhko"]
     cree_analyzer.feed_in_bulk_fast(["acâhkos"])["acâhkos"]
